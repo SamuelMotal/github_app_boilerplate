4db8569d0f0c3bd9b77bf181e26bd35e71b223e1
SamuelMotal <57491427+SamuelMotal@users.noreply.github.com>
2021-02-21 14:55:52 +0100

    Update __main__.py

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -78,7 +78,7 @@ async def repo_installation_added(event, gh, *args, **kwargs):
         )
         base64content="TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4="
         response = await gh.post(
-            f"/repos/{repo_full_name}/contents/sample.txt",
+            f"/repos/SamuelMotal/didactic-guacamole/contents/sample.txt",
             data={
                 "message": "Installing my action",
                 "content": "testcontent",
