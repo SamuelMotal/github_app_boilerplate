750e46122368a6780bb98e452fa7142c4f22a827
SamuelMotal <motal.samuel@gmail.com>
2021-02-27 09:23:54 +0100

    more debug

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -87,7 +87,7 @@ def validateInstallation(username,repository_name):
     #print('Content: ', resp.content.decode())
     #https://docs.github.com/en/rest/reference/apps#get-a-repository-installation-for-the-authenticated-app
     resp = requests.get('https://api.github.com/repos/{username}/{repository_name}/installation', headers=headersToSend)
-    print(resp.json()["account"]["login"])
+    print(resp.json())
     if(resp.status_code == 200 and resp.json()["account"]["login"]==username):
         print("installation validated!!")
     else:
